b743a54b4d2097fbe9085b61a2d3d66058e71a03
Akbar <akbarali1klr@gmail.com>
2019-01-25 21:26:59 +0530

    script completed to add questions to db

---
+++ backend/scripts/insertQuestions/index.js
@@ -0,0 +1,1 @@
+require('./insertQuestions.js')();
---
+++ backend/scripts/insertQuestions/test/script.tests.js
@@ -0,0 +1,13 @@
+let expect = require('chai').expect,
+    script = require('../insertQuestions.js');
+
+describe('main', function() {
+	it('Inserts questions in database', function(done) {
+		script()
+		.then((data)=> {
+			expect(data).to.equal(25)
+			done()
+		})
+	});
+});
+
